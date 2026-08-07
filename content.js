e2d690b2269f6920f7e2d76ede6ed191330f0432
taliaduckie <129779707+taliaduckie@users.noreply.github.com>
2026-08-06 21:50:29 -0700

    fix cap

--- content.js
+++ content.js
@@ -16,7 +16,7 @@
 
 const SITE = {
   sun: {
-    label: 'hi!',   // <- shown under the sun
+    label: 'Hi!',   // <- shown under the sun
     desc:  'click to read more',
     color: '#c47a2f',
     r: 46,
